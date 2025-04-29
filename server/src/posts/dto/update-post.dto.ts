d2865438c49030f404899ee5d94a02450feb9f14
A7med-zaki <ahmedzaki123789@gmail.com>
2025-04-29 22:59:33 +0300

    backend

--- server/src/posts/dto/update-post.dto.ts
+++ server/src/posts/dto/update-post.dto.ts
@@ -1,4 +1,11 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePostDto } from './create-post.dto';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  body: string;
+}
--- server/src/utils/image-validation.pipe.ts
+++ server/src/utils/image-validation.pipe.ts
@@ -3,7 +3,7 @@ import { MaxFileSizeValidator, FileTypeValidator, ParseFilePipe } from '@nestjs/
 export const imageValidationPipe = new ParseFilePipe({
   validators: [
     new MaxFileSizeValidator({ maxSize: 2 * 1024 * 1024 }),
-    new FileTypeValidator({ fileType: /(jpg|jpeg|png)$/ }),
+    new FileTypeValidator({ fileType: 'image/*' }),
   ],
   fileIsRequired: true,
 });
